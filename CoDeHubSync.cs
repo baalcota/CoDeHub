ae005fb59712a9b9e3b54182e9b47d60e093dd6f
baalcota <baalcota@gmail.com>
2026-06-11 23:01:40 +0200

    Add project files.

---
+++ CoDeHubSync.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Security.Cryptography.X509Certificates;
+using Eto.Forms;
+
+using Rhino;
+using Rhino.Commands;
+using Rhino.Input;
+using Rhino.Input.Custom;
+
+namespace CoDeHub
+{
+    public class CoDeHubSync : Rhino.Commands.Command
+    {
+        public CoDeHubSync()
+        {
+            Instance = this;
+        }
+        public static CoDeHubSync Instance { get; private set; }
+
+        public override string EnglishName => "CoDeHubSync";
+
+        protected override Result RunCommand(RhinoDoc doc, RunMode mode)
+        {
+            // CoDeHub Folder Directory
+            string tyrDir = "O:\\STH\\923096\\CoDe Hub\\Snippets";
+
+            try
+            {
+                // Get User Local folder (user objects). Independent of Rhino version (7.0, 8.0, etc...)
+                string rhinoDir = Rhino.RhinoApp.GetDataDirectory(true, false);
+                string[] list_path = rhinoDir.Split(new string[] { "McNeel" }, StringSplitOptions.None);
+                string localDir = list_path[0] + "Grasshopper\\UserObjects";
+
+                // Filter .ghuser files
+                string[] ghuser_files = Directory.GetFiles(tyrDir, "*.ghuser");
+                
+                if (ghuser_files.Length > 0)
+                {
+                    foreach (string ghu_file in ghuser_files)
+                    {
+                        // Get User Object file Name
+                        string fName = Path.GetFileName(ghu_file);
+                        try
+                        {
+                            // Copy file from TyrDir --> LocalDir (overwrites identical files!)
+                            File.Copy(Path.Combine(tyrDir, fName), Path.Combine(localDir, fName), true);
+                            Rhino.RhinoApp.WriteLine(fName+" downloaded successfully");
+                        }
+                        catch
+                        {
+                            return Result.CancelModelessDialog;
+                        }
+                    }
+
+                    return Result.Success;
+                }
+                else 
+                {
+                    Rhino.RhinoApp.WriteLine("No User Object were found in CoDe Hub");
+                    return Result.CancelModelessDialog; 
+                }
+            }
+            catch
+            {
+                Rhino.RhinoApp.WriteLine("Unsuccessful Sync, contact CoDe Hub");
+                return Result.CancelModelessDialog;
+            }
+        }
+    }
+}
---
+++ CoDeHubUpload.cs
@@ -0,0 +1,71 @@
+﻿using Eto.Forms;
+using Rhino;
+using Rhino.Commands;
+using Rhino.UI;
+using System;
+using System.IO;
+using System.Linq;
+using System.Xml.Linq;
+
+namespace CoDeHub
+{
+    public class CoDeHubUpload : Rhino.Commands.Command
+    {
+        public CoDeHubUpload()
+        {
+            Instance = this;
+        }
+
+        ///<summary>The only instance of this command.</summary>
+        public static CoDeHubUpload Instance { get; private set; }
+
+        public override string EnglishName => "CoDeHubUpload";
+
+        protected override Result RunCommand(RhinoDoc doc, RunMode mode)
+        {
+            //Eto.Forms.SaveFileDialog saveFileDialog = new Eto.Forms.SaveFileDialog();
+            //saveFileDialog.CurrentFilter = "Text Files (*.txt)|*.txt|All Files (*.*)|*.*";
+            //saveFileDialog.Title = "Save Your File";
+
+            // CoDeHub Folder Directory
+            string tyrDir = "O:\\STH\\923096\\CoDe Hub\\Snippets";
+
+            // Get User Local folder (user objects). Independent of Rhino version (7.0, 8.0, etc...)
+            string rhinoDir = Rhino.RhinoApp.GetDataDirectory(true, false);
+            string[] list_path = rhinoDir.Split(new string[] { "McNeel" }, StringSplitOptions.None);
+            string localDir = list_path[0] + "Grasshopper\\UserObjects";
+
+            var openDialog = new Eto.Forms.OpenFileDialog()
+            {
+                Directory = new Uri(localDir),
+                Filters = { new FileFilter("Grasshopper User File (*.ghuser)", "*.ghuser") },
+                CurrentFilterIndex = 0,
+                CheckFileExists = true,
+                MultiSelect = true,
+                Title = "Select UserObjects to upload"
+            };
+
+            var result = openDialog.ShowDialog(RhinoEtoApp.MainWindow);
+            
+            if (result == DialogResult.Ok)
+            {
+                // Get User Object files in local folder
+                foreach (string ghu_file in openDialog.Filenames) 
+                {
+                    try
+                    {
+                        string fName = Path.GetFileName(ghu_file);
+
+                        // Copy file from LocalDir --> TyrDir (overwrites identical files!)
+                        File.Copy(Path.Combine(localDir, fName), Path.Combine(tyrDir, fName), true);
+                        Rhino.RhinoApp.WriteLine(fName + " uploaded successfully");
+                    }
+                    catch { return Result.CancelModelessDialog; }
+                }
+
+            }
+
+            return Result.Success;
+        }
+    }
+}
